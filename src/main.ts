7a83c9280b2f3c3959f12e8956739ec8e9dd7f54
Ekanui <nitisak2893@gmail.com>
2026-03-25 12:03:39 +0700

    Fix app entry and router base for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
-import app from '../App.vue'
-import antd from 'ant-design-vue'
+import App from './App.vue'
+import Antd from 'ant-design-vue'
 import router from './router'
-import "ant-design-vue/dist/reset.css"
+import 'ant-design-vue/dist/reset.css'
 
-createApp(app)
-.use(antd)
-.use(router)
-
-.mount('#app')
+createApp(App)
+	.use(Antd)
+	.use(router)
+	.mount('#app')
 
